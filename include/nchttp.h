3847e060ae0e3d0341f0a306ea5837f19cc864e3
Dennis Heimbigner <dmh@ucar.edu>
2019-01-01 16:43:41 -0700

    fileoffset

--- include/nchttp.h
+++ include/nchttp.h
@@ -8,9 +8,6 @@
 #ifndef NCHTTP_H
 #define NCHTTP_H
 
-/* Avoid use of off_t or off64_t */
-typedef long long fileoffset_t;
-
 extern int nc_http_open(const char* objecturl, void** curlp, fileoffset_t* filelenp);
 extern int nc_http_read(void* curl, const char* url, fileoffset_t start, fileoffset_t count, NCbytes* buf);
 extern int nc_http_close(void* curl);
